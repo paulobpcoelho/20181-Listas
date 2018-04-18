41511a89709a7799911262fe8d1396bc68253c3f
paulobpcoelho <paulocoelhobp@gmail.com>
2018-04-18 00:19:17 -0300

    Add files via upload

---
+++ Lista3Exercicio2.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lista3exercicio2;
+
+/**
+ *
+ * @author paulo
+ */
+public class Lista3Exercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        for (int i = 0; i <= 9; i++) {
+
+            for (int t = 0; t <= 9; t++) {
+
+                System.out.println(i + "x" + t + "=" + i * t);
+            }
+        }
+    }
+
+}
